8beb9fca4081ccb926c0cefc90e79596a88b6a10
Unknown <vya5@sfu.ca>
2018-11-08 00:21:09 -0800

    Responsive

--- js/animate.js
+++ js/animate.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
+	
+	// Fade out menu button
+	/*
 	$(".open-nav").click(function(){
         $(".open-nav").fadeOut(100);
     });
     $(".close-nav").click(function(){
         $(".open-nav").fadeIn(700);
     });
+	*/
+	
+	$(".overlay").hide();
+	
 })
 
 function openNav() {
-    document.getElementById("navbar").style.width = "14vw";
+	document.getElementById("navbar").style.width = "8rem";
+	$(".overlay").fadeIn(200);
 }
 
 function closeNav() {
     document.getElementById("navbar").style.width = "0";
+	$(".overlay").fadeOut(200);
 }
