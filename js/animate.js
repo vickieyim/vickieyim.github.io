bee8b6f00d99f57574c7cc7358d072039190a415
vickieyim <vya5@sfu.ca>
2019-02-16 16:05:40 -0800

    New Portfolio

--- js/animate.js
+++ js/animate.js
@@ -1,55 +1,6 @@
+var $;
+var document;
+
 $(document).ready(function() {
-
-	// Fade out menu button
-	/*
-	$(".open-nav").click(function(){
-        $(".open-nav").fadeOut(100);
-    });
-    $(".close-nav").click(function(){
-        $(".open-nav").fadeIn(700);
-    });
-	*/
-
-	$(".overlay").hide();
-
-})
-
-function openNav() {
-	document.getElementById("navbar").style.width = "8rem";
-	$(".overlay").fadeIn(200);
-}
-
-function closeNav() {
-	document.getElementById("navbar").style.width = "0";
-	$(".overlay").fadeOut(200);
-}
-
-/* --------------------- Slideshow / Carousel --------------------- */
-/* -- From https://www.w3schools.com/howto/howto_js_slideshow.asp -- */
-/*
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-	showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-	showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-	var i;
-	var slides = document.getElementsByClassName("slides");
-	var dots = document.getElementsByClassName("slides-dot");
-	if (n > slides.length) {slideIndex = 1} 
-	if (n < 1) {slideIndex = slides.length}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none"; 
-	}
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" slides-dot-active", "");
-	}
-	slides[slideIndex-1].style.display = "block"; 
-	dots[slideIndex-1].className += " slides-dot-active";
-}*/
+   $('.media').bind('contextmenu',function() { return false; });
+});
